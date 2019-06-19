a681f981432b0780edf59ea018e83eeebb05a593
Evan Schlesinger <esslushy@gmail.com>
2019-06-19 17:19:03 -0500

    finished tag api wrapper for python

--- threadfixpro.py
+++ threadfixpro.py
@@ -663,6 +663,89 @@ class ThreadFixProAPI(object):
         """
         return self._request('POST', 'rest/scans/' + str(scan_id) + '/metadata/' + str(scan_metadata_key_id) + '/delete')
 
+    # Tags
+
+    def create_tag(self, name, tag_type="APPLICATION"):
+        """
+        Creats a new tag with the given name
+        :param name: Name to assign the new tag. 60 character limit
+        :param tag_type: The type of tag to create
+        """
+        params = {'name' : name, 'tagType' : tag_type}
+        return self._request('POST', 'rest/tags/new', params)
+
+    def get_tag_by_id(self, tag_id):
+        """
+        Gets tag by the given tagId
+        :param tag_id: Tag identifier
+        """
+        return self._request('GET', 'rest/tags/' + str(tag_id))
+
+    def get_tag_by_name(self, tag_name):
+        """
+        Gets tag by the given name
+        :param tag_name: The name of a tag to be gotten
+        """
+        return self._request('GET', 'rest/tags/lookup?name=' + str(tag_name))
+
+    def get_tags_by_vulnerability(self, vuln_id):
+        """
+        Gets tags attached to a given vulnerability
+        :param vuln_id: The identifier of the vulnerability to get the tags from
+        """
+        return self._request('GET', 'rest/tags/vulnerabilities' + str(vuln_id))
+
+    def get_all_tags(self):
+        """
+        Returns a list of all tags and returns their JSON
+        """
+        return self._request('GET', 'rest/tags/index')
+
+    def list_tags(self):
+        """
+        Retrieves a list of only tag names, ids, and types.
+        """
+        return self._request('GET', 'rest/tags/list')
+
+    def update_tag(self, tag_id, name):
+        """
+        Updates the name of the tag with the given tagId
+        :param tag_id: Tag identifier
+        :param name: New name to assign the tag
+        """
+        params = {'name' : name}
+        return self._request('POST', 'rest/tags/' + str(tag_id) + '/update', params)
+
+    def add_tag_to_application(self, application_id, tag_id):
+        """
+        Attaches the tag with the given tagId to the app with the given appId
+        :param application_id: Application identifier
+        :param tag_id: Tag identifier
+        """
+        return self._request('POST', 'rest/applications/' + str(application_id) + '/tags/add/' + str(tag_id))
+
+    def remove_tag_to_application(self, application_id, tag_id):
+        """
+        Removes the tag with the given tagId to the app with the given appId
+        :param application_id: Application identifier
+        :param tag_id: Tag identifier
+        """
+        return self._request('POST', 'rest/applications/' + str(application_id) + '/tags/remove/' + str(tag_id))
+
+    def delete_tag(self, tag_id):
+        """
+        Deletes the tag with the given tagId
+        :params tag_id: Tag identifier
+        """
+        return self._request('POST', 'rest/tags/' + str(tag_id) + '/delete')
+
+    def list_applications_for_tag(self, tag_id):
+        """
+        Returns the JSON of the apps that have the tag with the given tagId
+        :params tag_id: Tag identifier
+        """
+        return self._request('GET', 'rest/tags/' + str(tag_id) + '/listApplications')
+
     # Tasks
 
     def queue_scan(self, application_id, scanner_name, target_url = None, scan_config_id = None):
