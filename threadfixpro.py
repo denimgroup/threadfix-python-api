a5cd2f6678505f269c9db93760ec01ee9e1b63cb
Evan Schlesinger <esslushy@gmail.com>
2019-06-19 16:57:53 -0500

    finished scan api wrapper for python

--- threadfixpro.py
+++ threadfixpro.py
@@ -22,7 +22,7 @@ class ThreadFixProAPI(object):
     def __init__(self, host, api_key, verify_ssl=True, timeout=30, user_agent=None, cert=None, debug=False):
         """
         Initialize a ThreadFix Pro API instance.
-        :param host: The URL for the ThreadFix Pro server. (e.g., http://localhost:8080/threadfix/) NOTE: must include http://
+        :param host: The URL for the ThreadFix Pro server. (e.g., http://localhost:8080/threadfix/) NOTE: must include http:// TODO: make it so that it is required or implicitly added if forgotten
         :param api_key: The API key generated on the ThreadFix Pro API Key page.
         :param verify_ssl: Specify if API requests will verify the host's SSL certificate, defaults to true.
         :param timeout: HTTP timeout in seconds, default is 30.
@@ -564,6 +564,20 @@ class ThreadFixProAPI(object):
 
     # Scans
 
+    def get_scan_details(self, scan_id):
+        """
+        List all scans for a given application
+        :param scan_id: Scan identifier.
+        """
+        return self._request('GET', 'rest/scans/' + str(scan_id))
+
+    def list_scans(self, application_id):
+        """
+        List all scans for a given application
+        :param application_id: Application identifier.
+        """
+        return self._request('GET', 'rest/applications/' + str(application_id) + '/scans')
+
     def upload_scan(self, application_id, file_path):
         """
         Uploads and processes a scan file.
@@ -574,20 +588,26 @@ class ThreadFixProAPI(object):
             'POST', 'rest/applications/' + str(application_id) + '/upload',
             files={'file': open(file_path, 'rb')}
         )
-
-    def list_scans(self, application_id):
+    
+    def multiple_file_scan_upload(self, application_id, file_paths, bulk_upload=False):
         """
-        List all scans for a given application
+        Uploads and processes multiple scan file.
         :param application_id: Application identifier.
+        :param file_path: Path to the scan file to be uploaded.
+        :param bulk_upload: Upload files as a single scan (False) or separate scans (True)
         """
-        return self._request('GET', 'rest/applications/' + str(application_id) + '/scans')
+        return self._request(
+            'POST', 'rest/applications/' + str(application_id) + '/upload/multi',
+            files= [{'file' : open(file_path)} for file_path in file_paths]
+        )
 
-    def get_scan_details(self, scan_id):
+    def check_pending_scan_status(self, application_id, scan_id):
         """
-        List all scans for a given application
-        :param scan_id: Scan identifier.
+        Check the status of a scan after it has been queued
+        :param application_id: Application identifier
+        :param scan_id: Scan identifier
         """
-        return self._request('GET', 'rest/scans/' + str(scan_id))
+        return self._request('GET', 'rest/applications/' + str(application_id) + '/pendingScan/' + str(scan_id) + '/status')
 
     def download_scan(self, scan_id, filename):
         """
@@ -597,6 +617,51 @@ class ThreadFixProAPI(object):
         """
         return self._request('GET', 'rest/scans/' + str(scan_id) + '/download',
                              params={'scanFileName': filename})
+
+    def delete_scan(self, scan_id):
+        """
+        Queues the specified scan for deletion
+        :param scan_id: Scan identifier
+        """
+        return self._request('DELETE', 'rest/scans/' + str(scan_id) + '/delete')
+
+    def edit_scan_metadata(self, metadata_key_id, key=None, description=None, title=None):
+        """
+        Updates scan metadata
+        :param metadata_key_id:
+        :param key: The scan metadata key for the metadata which will be edited
+        :param description: New text for description field
+        :param title: Scan Metadata key title. Used if key param is not present
+        """
+        params = {}
+        if key:
+            params['key'] = key
+        if description:
+            params['description'] = description
+        if title:
+            params['title'] = title
+        return self._request('POST', 'rest/customize/scanmetadata/keys/' + str(metadata_key_id) + '/update', params)
+
+    def create_scan_metadata(self, scan_id, key, description, title=None):
+        """
+        Creates new scan metadata
+        :param scan_id: Scan identifier
+        :param key: The metadata key ID
+        :param description: Text description of metadata
+        :param title: The scan metadata key title.
+        """
+        params = {'key' : key, 'description' : description}
+        if title:
+            params['title'] = title
+        return self._request('POST', 'rest/scans/' + str(scan_id) + '/metadata/new', params)
+
+    def delete_scan_metadata(self, scan_id, scan_metadata_key_id):
+        """
+        Deletes scan metadata from scan
+        :param scan_id: Scan identifier
+        :param scan_metadata_key_id: Scan Metadata Key identifier
+        """
+        return self._request('POST', 'rest/scans/' + str(scan_id) + '/metadata/' + str(scan_metadata_key_id) + '/delete')
 
     # Tasks
 
