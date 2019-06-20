560d8e2208d60c6abcbaae95f995cc1268633924
Evan Schlesinger <esslushy@gmail.com>
2019-06-20 16:09:03 -0500

    finished remote providers api wrapping

--- threadfixpro.py
+++ threadfixpro.py
@@ -1422,6 +1422,101 @@ class ThreadFixProAPI(object):
         :param app_defect_tracker_id: App Defect Tracker identifier
         """
         return self._request('PUT', 'rest/cicd/defectReporting/' + str(defect_reporter_id) + '/removeApplicationDefectTracker/' + str(app_defect_tracker_id))
+    
+    # Remote Providers
+
+    def add_remote_provider_application_mapping(self, remote_provider_application_id, application_id, report_url=None):
+        """
+        Maps a ThreadFix application to the Remote Provider application
+        :param remote_provider_application_id: Remote Provider Application identifier
+        :param application_id: Application identifier
+        :param report_url: The version URL to use for Black Duck remote provider application
+        """
+        params = {'applicationId' : application_id}
+        if report_url:
+            params['reportUrl'] = report_url
+        return self._request('POST', 'rest/remoteprovider/remoteProviderApplication/' + str(remote_provider_application_id) + '/addMapping', params)
+
+    def check_remote_provider_application_import_status(self, remote_provider_application_id):
+        """
+        Returns the status for the Remote Provider Application import
+        :param remote_provider_application_id: Remote Provider Application identifier
+        """
+        return self._request('GET', 'rest/remoteprovider/remoteProviderApplication/' + str(remote_provider_application_id) + '/status')
+
+    def get_remote_provider_application_versions(self, remote_provider_application_id):
+        """
+        Returns the status for the Remote Provider Application import
+        :param remote_provider_application_id: Remote Provider Application identifier
+        """
+        return self._request('GET', 'rest/remoteprovider/remoteProviderApplication/' + str(remote_provider_application_id) + '/versions')
+
+    def get_remote_provider_applications(self, team_id=None, remote_provider_id=None, application_id=None, hide_mapped=None, hide_unmapped=None):
+        """
+        Returns a list of Remote Provider applications as well as the applications they are mapped to
+        :param team_id: Team identifier. Returns only results mapped to that team
+        :param remote_provider_id: Remote Provider identifier. Return only results that belong to the specified Remote Provider
+        :param application_id: Application identifier. Return only results that belong to the specified application
+        :param hide_mapped: Does not return results that are already mapped to ThreadFix applications
+        :param hide_unmapped: Does not return results that lack a ThreadFix application mapping
+        """
+        params = {}
+        if team_id:
+            params['teamId'] = team_id
+        if remote_provider_id:
+            params['remoteProviderId'] = remote_provider_id
+        if application_id:
+            params['applicationId'] = application_id
+        if hide_mapped:
+            params['hideMapped'] = hide_mapped
+        if hide_unmapped:
+            params['hideUnmapped'] = hide_unmapped
+        return self._request('GET', 'rest/remoteprovider/remoteProviderApplication/list', params)
+
+    def get_remote_provider_applications_by_name(self, name, remote_provider_id):
+        """
+        Gets list of Remote Provider Applications with the provided name
+        :param name: Name of the Remote Provider Applications to return
+        :param remote_provider_id: ID of a Remote Provider to restrict the search to
+        """
+        params = {'name' : name}
+        if remote_provider_id:
+            params['remoteProviderId'] = remote_provider_id
+        return self._request('GET', 'rest/remoteprovider/remoteProviderApplication/search', params)
+
+    def get_remote_providers(self):
+        """
+        Returns a list of Remote Providers
+        """
+        return self._request('GET', 'rest/remoteprovider/list')
+
+    def import_remote_provider_scans(self, remote_provider_application_id):
+        """
+        Triggers a Remote Provider import for the specified Remote Provider Application
+        :param remote_provider_application_id: Remote Provider Application identifier
+        """
+        return self._request('POST', 'rest/remoteprovider/remoteProviderApplication/' + str(remote_provider_application_id) + '/import')
+
+    def import_remote_provider_all(self, remote_provider_id):
+        """
+        Triggers a Remote Provider import for all applications from the specified Remote Provide
+        :param remote_provider_id: Remote Provider identifier
+        """
+        return self._request('POST', 'rest/remoteprovider/' + str(remote_provider_id) + '/import')
+
+    def remove_remote_provider_application_mapping(self, remote_provider_application_id):
+        """
+        Removes the ThreadFix application mapping for a Remote Provider application
+        :param remote_provider_application_id: Remote Provider Application identifier
+        """
+        return self._request('DELETE', 'rest/remoteprovider/remoteProviderApplication/' + str(remote_provider_application_id) + '/removeMapping')
+
+    def sync_remote_provider_applications(self, remote_provider_id):
+        """
+        Syncs ThreadFix Remote Provider Applications for the specified Remote Provider adding new ones and removing any that are no longer present in the Remote Provide
+        :param remote_provider_id: Remote Provider identifier
+        """
+        return self._request('POST', 'rest/remoteprovider/' + str(remote_provider_id) + '/sync')
 
     # Utility
 
