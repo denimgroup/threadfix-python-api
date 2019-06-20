092308830d2d7da3f8c0ff7e37709b8b3c6a7659
Evan Schlesinger <esslushy@gmail.com>
2019-06-20 17:40:59 -0500

    finished email api

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import threadfixpro
 
 api = threadfixpro.ThreadFixProAPI('http://localhost:8081/threadfix/', '5bi08XUPA9wdTEnS0wnNx8SaD8llyfwCg8O2SBetGyF4', verify_ssl=False, debug=True)#make api handler note url must end in a slash
 
-print(api.create_application_version('test89', '2019-06-20', 1))
+print(api.get_metadata_keys())
 
--- threadfixpro.py
+++ threadfixpro.py
@@ -1524,6 +1524,90 @@ class ThreadFixProAPI(object):
 
     # Email Reporting
 
+    def list_scheduled_email_reports(self):
+        """
+        Retrives all scheduled email reports
+        """
+        return self._request('GET', 'rest/scheduledEmailReport/list')
+
+    def create_scheduled_email_report(self, team_id, severity, frequency, day, hour, minute, period, cron_expression, scheduling_method=None):
+        """
+        Creates a new scheduled email report
+        :param team_id: The ID for the team to attach to the report
+        :param severity: The name of the severity for the report's severity threshold
+        :param frequency: Must be "Weekly" or "Daily" Determines if the schedule will be everyday or only on a specified day of the week. "SELECT" method requires this
+        :param day: Only used if it is on Weekly frequency. The day on which the report will execute. "SELECT" method requires this
+        :param hour: The hour 1-12 for the time the report will execute. "SELECT" method requires this
+        :param minute: 0, 15, 30, 45 - The minute the report will execute. "SELECT" method requires this
+        :param period: "AM" or "PM" for the time the report will execute. "SELECT" method requires this
+        :param cron_expression: Only if using "CRON" scheduling method. Cron expression for when it will execure
+        :param scheduling_method: "CRON" or "SELECT", but will default to "SELECT"
+        """
+        params = {'teamIds' : team_id, 'severity' : severity, 'frequency' : frequency, 'day' : day, 'hour' : hour, 'minute' : minute, 'period' : period, 'cronExpression' : cron_expression}
+        if scheduling_method:
+            params['schedulingMethod'] = scheduling_method
+        return self._request('POST', 'rest/scheduledEmailReport/add', params)
+
+    def create_scheduled_email_report(self, scheduled_email_report_id, team_id, severity, frequency, day, hour, minute, period, cron_expression, scheduling_method=None):
+        """
+        Creates a new scheduled email report
+        :param scheduled_email_report_id: Scheduled Email Report identifier
+        :param team_id: The ID for the team to attach to the report
+        :param severity: The name of the severity for the report's severity threshold
+        :param frequency: Must be "Weekly" or "Daily" Determines if the schedule will be everyday or only on a specified day of the week. "SELECT" method requires this
+        :param day: Only used if it is on Weekly frequency. The day on which the report will execute. "SELECT" method requires this
+        :param hour: The hour 1-12 for the time the report will execute. "SELECT" method requires this
+        :param minute: 0, 15, 30, 45 - The minute the report will execute. "SELECT" method requires this
+        :param period: "AM" or "PM" for the time the report will execute. "SELECT" method requires this
+        :param cron_expression: Only if using "CRON" scheduling method. Cron expression for when it will execure
+        :param scheduling_method: "CRON" or "SELECT", but will default to "SELECT"
+        """
+        params = {'teamIds' : team_id, 'severity' : severity, 'frequency' : frequency, 'day' : day, 'hour' : hour, 'minute' : minute, 'period' : period, 'cronExpression' : cron_expression}
+        if scheduling_method:
+            params['schedulingMethod'] = scheduling_method
+        return self._request('PUT', 'rest/scheduledEmailReport/' + str(scheduled_email_report_id) + '/edit', params)
+
+    def delete_scheduled_email_report(self, scheduled_email_report_id):
+        """
+        Deletes a scheduled email report
+        :param scheduled_email_report_id: Scheduled Email Report identifier
+        """
+        return self._request('DELETE', 'rest/scheduledEmailReport/' + str(scheduled_email_report_id))
+
+    def set_emails_for_scheduled_email_report(self, scheduled_email_report_id, emails=None):
+        """
+        Sets the emails attached to an existing scheduled email report
+        :param scheduled_email_report_id: Scheduled Email Report identifier
+        :param emails: 	The emails to attach to the Schedule Email Report.  See the example for how to attach multiple.  
+                        To remove all emails from a report, make this call without providing any 'emails' parameters.
+        """
+        params = {}
+        if emails:
+            params['emails'] = emails
+        return self._request('PUT', 'rest/scheduledEmailReport/' + str(scheduled_email_report_id) + '/emails', params)
+
+    def add_email_list_to_scheduled_email_report(self, scheduled_email_report_id, email_list_id):
+        """
+        Attaches an email list to a scheduled email report
+        :param scheduled_email_report_id: Scheduled Email Report identifier
+        :param email_list_id: Email List identifier
+        """
+        return self._request('POST', 'rest/scheduledEmailReport/' + str(scheduled_email_report_id) + '/emailList/' + str(email_list_id))
+
+    def remove_email_list_to_scheduled_email_report(self, scheduled_email_report_id, email_list_id):
+        """
+        Removes an email list to a scheduled email report
+        :param scheduled_email_report_id: Scheduled Email Report identifier
+        :param email_list_id: Email List identifier
+        """
+        return self._request('DELETE', 'rest/scheduledEmailReport/' + str(scheduled_email_report_id) + '/emailList/' + str(email_list_id))
+
+    def list_email_lists(self):
+        """
+        Retrieves all email lists
+        """
+        return self._request('GET', 'rest/emailList/list')
+
     # Miscellaneous
 
     def clear_global_fpr_filter_set_override(self):
@@ -1626,7 +1710,7 @@ class ThreadFixProAPI(object):
             params['active'] = active
         return self._request('POST', 'rest/metadataKeys/' + str(metadata_key_id) + '/update', params)
 
-    def get_metadata_key(self, keytype=None):
+    def get_metadata_keys(self, keytype=None):
         """
         Returns all metadata keys
         :param keytype: Type of Metadata key to return
