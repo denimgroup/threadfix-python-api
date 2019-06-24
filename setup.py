fcade9aab63ebe8bee7e2060a594a929fcd8f3a2
Evan Schlesinger <esslushy@gmail.com>
2019-06-24 10:39:38 -0500

    Added version requirements to make sure no outdated packages are used which may affect compatibility

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=['ThreadFixPythonApi', '_utils',],
     license='MIT',
     long_description='A python implementation of ThreadFix\'s API for easier use with python. Built off of original work by (c) 2018 Target Brands, Inc.',
-    install_requires=['requests', 'urllib3']
+    install_requires=['requests>=2.22.0', 'urllib3>=1.25.3']
 )
