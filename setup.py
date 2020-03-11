41b9bc39f106ee30bd90306e2cb55ef78b83d5f9
Dan Cornell <dan@denimgroup.com>
2020-03-11 13:11:54 -0500

    Fixed package naming to be consistent with the original Cleaned up packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+import setuptools
 from distutils.core import setup
 
 setup(
-    name='ThreadFixPythonAPI',
-    version='1.0.7',
-    packages=['ThreadFixPythonApi', '_utils',],
+    name='ThreadFixProAPI',
+    version='1.0.8',
+    packages=['ThreadFixProApi', '_utils',],
     license='MIT',
     long_description='A python implementation of ThreadFix\'s API for easier use with python. Built off of original work by (c) 2018 Target Brands, Inc.'
 )
