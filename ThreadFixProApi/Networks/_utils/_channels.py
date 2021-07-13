d1c46202ca5e338783a4a8b7d053cdbd0f5c0830
Evan Schlesinger <esslushy@gmail.com>
2021-07-13 16:50:09 -0500

    finished channels api

--- ThreadFixProApi/Networks/_utils/_channels.py
+++ ThreadFixProApi/Networks/_utils/_channels.py
@@ -22,3 +22,23 @@ class ChannelsAPI(API):
         :param debug: Prints requests and responses, useful for debugging.
         """
         super().__init__(host, api_key, verify_ssl, timeout, headers, user_agent, cert, debug)
+
+    def fetch_all_channels(self, page=1, limit=50, href=None):
+        """
+        Fetches all channels one page at a time of limit 
+        :param page: The page of the channels to get (optional if you have href)
+        :param limit: The amount of channels per page
+        :param href: The link to the next page in the system from a previous call
+        """
+        # If href (calling another page gives an href tag for next page in line)
+        if href:
+            return super().request('GET', '/api/network' + href)
+        # First call
+        return super().request('GET', f'/api/network/channels?_page={page}&_limit={limit}')
+
+    def find_channel_by_id(self, channel_id):
+        """
+        Gets a channel by its id
+        :param channel_id: ID of the channel to get
+        """
+        return super().request('GET', f'/api/network/channels/{channel_id}')
