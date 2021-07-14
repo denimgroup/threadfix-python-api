c585d8b9ae06a105a7f23a314bb0005d39c77569
Evan Schlesinger <esslushy@gmail.com>
2021-07-14 12:39:53 -0500

    fixed typo in search api

--- ThreadFixProApi/Networks/_utils/_search.py
+++ ThreadFixProApi/Networks/_utils/_search.py
@@ -142,5 +142,5 @@ class SearchAPI(API):
         """
         params  = { 'isArchived' : is_archived }
         if href:
-            super().request('GET', f'/api/search/assets/{network_id}' + href, params=params)
-        return super().request('GET', f'/api/search/assets/{network_id}/networks?_page={page}&_limit={limit}', params=params)
+            super().request('GET', f'/api/search/network/{network_id}' + href, params=params)
+        return super().request('GET', f'/api/search/network/{network_id}/networks?_page={page}&_limit={limit}', params=params)
